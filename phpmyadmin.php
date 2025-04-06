406797460acbf93035fa6931e040d842940e83bc
jaismeetcoder45 <gstsinghhardeep@gmail.com>
2025-04-06 22:35:54 +0530

    Add files via upload

---
+++ phpmyadmin.php
@@ -0,0 +1,26 @@
+CREATE TABLE users (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+CREATE TABLE journal (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    entry TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE mood_tracker (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    mood VARCHAR(50),
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE todos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    task TEXT NOT NULL,
+    is_completed BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
